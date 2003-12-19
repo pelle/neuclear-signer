59fbbd5de41d7ea314368ed414ddc305cb8bd662
pelle <pelle>
2003-12-19 00:30:55 +0000

    Lots of usability changes through out all the passphrase agents and end user tools.

--- src/java/org/neuclear/signers/standalone/StandaloneSigner.java
+++ src/java/org/neuclear/signers/standalone/StandaloneSigner.java
@@ -7,8 +7,11 @@ import org.mortbay.jetty.servlet.ServletHandler;
 import org.mortbay.jetty.Server;
 import org.mortbay.util.InetAddrPort;
 import org.mortbay.util.MultiException;
+import org.neuclear.commons.crypto.CryptoTools;
+import org.neuclear.id.tools.commandline.IdentityCreator;
 
 import java.io.IOException;
+import java.io.File;
 
 
 
@@ -19,20 +22,29 @@ import java.io.IOException;
  */
 public class StandaloneSigner {
     public static void main(String args[]){
-        try {
-            Server server = new Server();
-            server.addListener(new InetAddrPort("127.0.0.1",11870));
-            HttpContext context = server.getContext("/");
-            ServletHandler handler= new ServletHandler();
-            handler.addServlet("/Signer","org.neuclear.signers.standalone.StandaloneSigningServlet");
-            context.addHandler(handler);
-            server.start();
-            context.start();
-            handler.start();
-            handler.initializeServlets();
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use Options | File Templates.
+        if (args.length>0) {
+            IdentityCreator.main(args);
+            return;
         }
-
+        File keystore=new File(CryptoTools.DEFAULT_KEYSTORE);
+        if (!keystore.exists()) {
+            System.out.println("First you need to create an Identity. Use this tool with the following options.\n For more help go to http://neuclear.org/signer/bdg.html");
+            IdentityCreator.main(args);
+        } else {
+            try {
+                Server server = new Server();
+                server.addListener(new InetAddrPort("127.0.0.1",11870));
+                HttpContext context = server.getContext("/");
+                ServletHandler handler= new ServletHandler();
+                handler.addServlet("/Signer","org.neuclear.signers.standalone.StandaloneSigningServlet");
+                context.addHandler(handler);
+                server.start();
+                context.start();
+                handler.start();
+                handler.initializeServlets();
+            } catch (Exception e) {
+                e.printStackTrace();  //To change body of catch statement use Options | File Templates.
+            }
+        }
     }
 }
--- src/java/org/neuclear/signers/standalone/StandaloneSigningServlet.java
+++ src/java/org/neuclear/signers/standalone/StandaloneSigningServlet.java
@@ -3,7 +3,11 @@ package org.neuclear.signers.standalone;
 import org.neuclear.signers.servlet.SigningServlet;
 import org.neuclear.commons.crypto.signers.Signer;
 import org.neuclear.commons.crypto.signers.DefaultSigner;
+import org.neuclear.commons.crypto.signers.InvalidPassphraseException;
 import org.neuclear.commons.crypto.passphraseagents.GuiDialogAgent;
+import org.neuclear.commons.crypto.passphraseagents.InteractiveAgent;
+import org.neuclear.commons.crypto.passphraseagents.UserCancellationException;
+import org.neuclear.commons.crypto.CryptoException;
 import org.neuclear.commons.NeuClearException;
 import org.neuclear.commons.Utility;
 import org.neuclear.commons.servlets.ServletTools;
@@ -26,15 +30,23 @@ import java.io.PrintWriter;
  * To change this template use Options | File Templates.
  */
 public class StandaloneSigningServlet extends SigningServlet {
-    protected Signer createSigner(ServletConfig config) throws GeneralSecurityException, NeuClearException, IOException {
-        return new DefaultSigner(new GuiDialogAgent());
+    public StandaloneSigningServlet() {
+        this.agent = new GuiDialogAgent();
+    }
+
+    protected Signer createSigner(ServletConfig config) throws CryptoException {
+        try {
+            return new DefaultSigner(agent);
+        } catch (InvalidPassphraseException e) {
+            return createSigner(config);
+        }
     }
 
     protected String getTitle(){
         return "NeuClear Personal Signing Service";
     }
     public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-        if(!request.getRemoteAddr().equals("127.0.0.1"))
+        if(!request.getRemoteAddr().equals("127.0.0.2"))
             ((HttpServletResponse)response).sendError(500,"No external access allowed");
         super.service(request, response);
     }
@@ -45,4 +57,6 @@ public class StandaloneSigningServlet extends SigningServlet {
         ServletTools.printHeader(writer, request,"NeuClear Signing Servlet");
 
     }
+    private final InteractiveAgent agent;
+
 }
