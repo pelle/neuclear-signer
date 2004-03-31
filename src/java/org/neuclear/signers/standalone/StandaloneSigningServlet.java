eac3dbe0b5e8a05df3f616c9abed9034bddd4b8a
pelle <pelle>
2004-03-31 23:13:55 +0000

    misc changes

--- src/java/org/neuclear/signers/standalone/StandaloneSigningServlet.java
+++ src/java/org/neuclear/signers/standalone/StandaloneSigningServlet.java
@@ -1,24 +1,18 @@
 package org.neuclear.signers.standalone;
 
-import org.neuclear.signers.servlet.SigningServlet;
-import org.neuclear.commons.crypto.signers.Signer;
-import org.neuclear.commons.crypto.signers.DefaultSigner;
-import org.neuclear.commons.crypto.signers.InvalidPassphraseException;
 import org.neuclear.commons.crypto.passphraseagents.GuiDialogAgent;
 import org.neuclear.commons.crypto.passphraseagents.InteractiveAgent;
 import org.neuclear.commons.crypto.passphraseagents.UserCancellationException;
-import org.neuclear.commons.crypto.CryptoException;
-import org.neuclear.commons.NeuClearException;
-import org.neuclear.commons.Utility;
+import org.neuclear.commons.crypto.signers.DefaultSigner;
+import org.neuclear.commons.crypto.signers.InvalidPassphraseException;
+import org.neuclear.commons.crypto.signers.Signer;
 import org.neuclear.commons.servlets.ServletTools;
+import org.neuclear.id.signers.SigningServlet;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.security.GeneralSecurityException;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -42,7 +36,7 @@ public class StandaloneSigningServlet extends SigningServlet {
         }
     }
 
-    protected String getTitle(){
+    protected String getTitle() {
         return "NeuClear Personal Signing Service";
     }
 /*
@@ -55,10 +49,11 @@ public class StandaloneSigningServlet extends SigningServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter writer=response.getWriter();
-        ServletTools.printHeader(writer, request,"NeuClear Signing Servlet");
+        PrintWriter writer = response.getWriter();
+        ServletTools.printHeader(writer, request, "NeuClear Signing Servlet");
 
     }
+
     private final InteractiveAgent agent;
 
 }
